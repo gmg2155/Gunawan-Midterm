88e4c49ebebb1f494487f15b8c5d168a14cb298b
Grace <GraceMarcelina@Graces-MacBook-Air-2.local>
2016-11-08 01:18:51 -0500

    edited code

---
+++ Midterm Project/js/main.js
@@ -0,0 +1,8 @@
+function quizAnswer (){
+var sentenceChoice = prompt('Which sentence best conveys the message in the simplest way? a or b?');
+if (sentenceChoice ==='b'){
+	alert("Perfect! Good Job!");
+
+} else {
+	alert("Sorry, your answer is wrong. Please try again.");}
+}
