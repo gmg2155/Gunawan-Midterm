e3a38af9506083df8b379d0cbd5d3c0e0114d16a
Grace <GraceMarcelina@Graces-MacBook-Air-2.local>
2016-11-09 18:42:39 -0500

    added final files

--- Midterm Project/js/code.js
+++ Midterm Project/js/code.js
@@ -1,8 +1,26 @@
-function quizAnswer (){
+document.getElementById('buttonA').addEventListener('click',function quizAnswerA (){
 var sentenceChoice = prompt('Which sentence best conveys the message in the simplest way? a or b?');
 if (sentenceChoice ==='b'){
 	alert("Perfect! Good Job!");
 
 } else {
 	alert("Sorry, your answer is wrong. Please try again.");}
-}
+});
+
+document.getElementById('buttonB').addEventListener('click',function quizAnswerB (){
+var sentenceChoice = prompt('Which sentence best conveys the message in the simplest way? a or b?');
+if (sentenceChoice ==='b'){
+	alert("Perfect! Good Job!");
+
+} else {
+	alert("Sorry, your answer is wrong. Please try again.");}
+});
+
+document.getElementById('buttonDone').addEventListener('click',function challengeDone (){
+var sentenceChoice = prompt('Have you checked all the boxes after following all the steps? y or n?');
+if (sentenceChoice ==='y'){
+	alert("Congratulations, you are now a speech master!");
+
+} else {
+	alert("Please go back to the checklist and make sure you have finished all the steps.");}
+});
